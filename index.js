a2c2e119438d1d15f74fc2acbffc897b8913e4c7
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-07-15 08:00:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here:
+function driversWithRevenueOver(array,number){
+  const result = array.filter(arif => arif.revenue > number);
+  return result;
+}
+function driverNamesWithRevenueOver(array,number){
+  const result = array.filter(arif => arif.revenue > number);
+  const result2 =result.map(isim=>isim.name);
+  return result2;
+}
+
+function exactMatch(array,obj){
+  for (const key in obj){
+return array.filter(driver => driver[key] === obj[key]);
+}}
+
+
+function exactMatchToList(array, obj){
+const list = exactMatch(array, obj)
+return list.map(driver => driver.name)
+}
